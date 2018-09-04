423c0a3466083d7188b93892c65db1b816a8cbdf
Geoff Collis <gcollis@ymail.com>
2018-09-04 14:24:48 +0200

    add recursive method

--- max_sum_incr_subset.py
+++ max_sum_incr_subset.py
@@ -8,24 +8,28 @@
 # Need to find subsets: [1,101], [1,2,3,100], [1,2,3,4,5] implies recursive or iterative
 
 
-def recursion(L, i, l, AS, S):
+def recursion(v, L, sequence, array_of_sequences):
+    #     print('Down: {0}, List: {1}'.format(v, L))
     if len(L) == 0:
-        return None
-    elif i > len(L):
         return False
-    if i == l:
-        return True
-    else:
+    elif len(L) == 1:
+        x = L[0]
+        sequence.append(v)
+        sequence.append(x)
+        # print(sequence)
+        array_of_sequences.append(sequence.copy())
+    elif len(L) > 1:
+        x = L[0]
+        sequence.append(v)
+        if v > x:
+            # print(sequence)
+            array_of_sequences.append(sequence.copy())
+            sequence.pop()
 
-        print(AS)
-        print(S)
-        print('L: {0}, {1}'.format(L, i))
-        curnum = L[i]
-        nextnum = L[i + 1]
-        if nextnum > curnum:
-            recursion(L, i + 1, l, AS, S)
-        else:
-            recursion(L, i + 2, l, AS, S)
+        if recursion(x, L[1:], sequence, array_of_sequences):
+            return False
+    else:
+        return True
 
 
 def iteration(L):
@@ -35,29 +39,58 @@ def iteration(L):
     lastval = sequences[-1]
     for j in L[1:]:
         newval = j
-        # print('val:{0}, lastval:{1}, newval: {2}'.format(val, lastval, newval))
         if L[-1] == newval:
+            # save last sequence
             sequences.append(newval)
-            T = sequences.copy()
-            # print('Sequence {0}'.format(sequences))
-            array_of_sequences.append(T)
+            S = sequences.copy()
+            array_of_sequences.append(S)
         elif lastval < newval:
+            # sequence ascending
             sequences.append(newval)
-            # print('Append {0} <= {1}; sequences: {2}'.format(val, newval, sequences))
             lastval = newval
         else:
             if len(sequences) > 1:
-                # print('Pop: {0}'.format(lastval))
-                T = sequences.copy()
-                # print('Sequence {0}'.format(sequences))
-                array_of_sequences.append(T)
+                # need to save sequence and start a new one
+                S = sequences.copy()
+                array_of_sequences.append(S)
                 sequences.pop()
-                # print('Poping {0}; sequences: {1}'.format(lastval, sequences))
                 lastval = sequences[-1]
                 sequences.append(newval)
-                # print('Popend {0} <= {1}; sequences: {2}'.format(val, newval, sequences))
                 lastval = newval
 
+    result = {'sum': 0, 'sequence': None}
+    # compute the highest value
+    for x in array_of_sequences:
+        total = sum(x)
+        if total > result['sum']:
+            result['sum'] = total
+            result['sequence'] = x
+
+    return(result)
+
+
+def max_sum_subset_iterative(L):
+    if len(L) == 0:
+        return None
+    if len(L) == 1:
+        return L[1]
+    else:
+        return (iteration(L))
+
+
+def max_sum_subset_recursive(L):
+    if len(L) == 0:
+        return None
+    elif len(L) == 1:
+        total = L[0]
+        return {'sum': total, 'sequence': L}
+
+    sequence = []
+    array_of_sequences = []
+    recursion(L[0], L[1:], sequence, array_of_sequences)
+    # print(array_of_sequences)
+
+    # compute the highest value
     result = {'sum': 0, 'sequence': None}
     for x in array_of_sequences:
         total = sum(x)
@@ -68,19 +101,19 @@ def iteration(L):
     return(result)
 
 
-def max_sum_subset(L):
-    if len(L) == 0:
-        return None
-    if len(L) == 1:
-        return L[1]
-    else:
-        return (iteration(L))
-
-
 if __name__ == "__main__":
     data = []
     data.append([1, 101, 2, 3, 100, 4, 5])
     data.append([])
 
+    heading = 'Iterative'
+    print(heading)
+    print('=' * len(heading))
     for d in data:
-        print('data: {0} => {1}'.format(d, max_sum_subset(d)))
+        print('data: {0} => {1}'.format(d, max_sum_subset_iterative(d)))
+
+    heading = 'Recursive'
+    print(heading)
+    print('=' * len(heading))
+    for d in data:
+        print('data: {0} => {1}'.format(d, max_sum_subset_recursive(d)))
