9d57a032910aa6cb114be22666d0bc1b8e5c533d
Geoff Collis <gcollis@ymail.com>
2018-09-13 16:50:11 +0200

    refactoring...

--- random_entry_from_fortune_file.py
+++ random_entry_from_fortune_file.py
@@ -60,7 +60,7 @@ def cookie_list(fd):
     return L
 
 
-def get_fortune_text(fd, offset):
+def get_fortune_text_from_list(fd, offset):
     '''
     Extract lulti-line text from the file.
     :param fd: file handle
@@ -98,7 +98,7 @@ if __name__ == '__main__':
 
         cookies = cookie_list(f)
         random_offset = random.randrange(len(cookies))
-        print(get_fortune_text(f, cookies[random_offset]), end='')
+        print(get_fortune_text_from_list(f, cookies[random_offset]), end='')
 
 #         print('{0}'.format(cookies))
 #         print("'{0}', has {1:3d} lines ({2} Bytes)".format(filename, get_line_count(f), file_size))
