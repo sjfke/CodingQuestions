4ee6f1c6f009388fe64109f3d5998618b3f0cabe
Geoff Collis <gcollis@ymail.com>
2018-09-13 15:26:15 +0200

    fortune using an index

--- random_entry_from_fortune_file.py
+++ random_entry_from_fortune_file.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python3
 #
 import os
+import random
+import re
 import sys
+import locale
 
-# https://github.com/bmc/fortunes
-# https://docs.python.org/3/library/random.html
-# https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
-# https://docs.python.org/3/library/os.path.html#os.path.getsize
+# 1 https://github.com/bmc/fortunes
+# 2 https://docs.python.org/3/library/random.html
+# 3 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
+# 4 https://docs.python.org/3/library/os.path.html#os.path.getsize
+# 5 https://www.tutorialspoint.com/python3/python_reg_expressions.htm
+# 6 https://python-notes.curiousefficiency.org/en/latest/python3/text_file_processing.html
 
 
 def get_line_count(fd):
     line_count = 0
+    current_offset = fd.tell()  # save file cursor
+    fd.seek(0)
     for line in fd:
         # print(line, end='')
         line_count += 1
 
+    fd.seek(current_offset)  # restore file cursor
     return line_count
 
 
-def coookie_list(fd):
-    L = []
+def cookie_list(fd):
+    '''
+    Build a list of separator offset locations
+    :param fd: file handle
+    '''
+
     # %
     # "Acting is an art which consists of keeping the audience from coughing."
     # %
@@ -29,9 +41,47 @@ def coookie_list(fd):
     #         ― Time Bandits
     # %
 
-    for line in fd:
-        print(line, end='')
+    current_offset = fd.tell()  # save file cursor
+    fd.seek(0)  # start at the first byte
+
+    L = []
+
+    # Cannot use for..loop and .tell() method
+    while True:
+        line = fd.readline()
+        if not line:
+            break
+
+        # Record offset of separator
+        if re.match(r'^%$', line):
+            L.append(fd.tell())
+
+    fd.seek(current_offset)  # restore file cursor
     return L
+
+
+def get_fortune_text(fd, offset):
+    '''
+    Extract lulti-line text from the file.
+    :param fd: file handle
+    :param offset: bytes from the begining of the file
+    '''
+    current_offset = fd.tell()
+    fd.seek(offset)
+    cookie_text = fd.readline()
+
+    while True:
+        line = fd.readline()
+        if not line:
+            break
+        elif re.match(r'^%$', line):
+            break
+        else:
+            cookie_text += line
+
+    fd.seek(current_offset)
+
+    return cookie_text
 
 
 if __name__ == '__main__':
@@ -44,6 +94,11 @@ if __name__ == '__main__':
         print("OS error: {0}".format(err))
         sys.exit(1)
 
-    with open(filename) as f:
+    with open(filename, 'r') as f:
 
-        print("'{0}', has {1:3d} lines ({2} Bytes)".format(filename, get_line_count(f), file_size))
+        cookies = cookie_list(f)
+        random_offset = random.randrange(len(cookies))
+        print(get_fortune_text(f, cookies[random_offset]), end='')
+
+#         print('{0}'.format(cookies))
+#         print("'{0}', has {1:3d} lines ({2} Bytes)".format(filename, get_line_count(f), file_size))
