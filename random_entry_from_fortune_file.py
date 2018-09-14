68c285d00db45138b132ba94059bc0e9dafb2a6a
Geoff Collis <gcollis@ymail.com>
2018-09-14 15:10:37 +0200

    clean up imports and add missing docstrings

--- random_entry_from_fortune_file.py
+++ random_entry_from_fortune_file.py
@@ -4,7 +4,6 @@ import os
 import random
 import re
 import sys
-import locale
 
 # 1 https://github.com/bmc/fortunes
 # 2 https://docs.python.org/3/library/random.html
@@ -14,13 +13,20 @@ import locale
 # 6 https://python-notes.curiousefficiency.org/en/latest/python3/text_file_processing.html
 
 
-def get_line_count(fd):
+def get_line_count(fd, strict=False):
+    '''
+    Get number of lines in file
+    :param fd: file handle
+    :param strict: only report non-empty lines
+    '''
     line_count = 0
     current_offset = fd.tell()  # save file cursor
     fd.seek(0)
     for line in fd:
-        # print(line, end='')
-        line_count += 1
+        if strict and line:
+            line_count += 1
+        else:
+            line_count += 1
 
     fd.seek(current_offset)  # restore file cursor
     return line_count
@@ -116,8 +122,12 @@ def get_fortune_text_from_file(fd, offset):
 
 
 def get_file_size(textstr):
+    '''
+    Return file size in Bytes
+    :param textstr: file name
+    '''
     try:
-        return(os.path.getsize(textstr))
+        return os.path.getsize(textstr)
     except OSError as err:
         print("OS error: {0}".format(err))
         sys.exit(1)
